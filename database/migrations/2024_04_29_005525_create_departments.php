b28c6c659b533c1a33c1a92c41813512fb1bdf87
WardsssGIT <kristianrj02@gmail.com>
2024-05-01 12:19:16 +0800

    departmentv3

--- database/migrations/2024_04_29_005525_create_departments.php
+++ database/migrations/2024_04_29_005525_create_departments.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
-            
             $table->unsignedBigInteger('report_table_id');
             $table->foreign('report_table_id')->references('id')->on('report_table');
+
+            $table->string('department_types')->nullable();
 
 
             $table->timestamps();
